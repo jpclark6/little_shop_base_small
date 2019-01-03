70e71e2365823183bc0cc232b079f7beac411966
jpclark6 <jpclark6@gmail.com>
2019-01-03 10:01:53 -0700

    blank additional coupon feature tests added

--- spec/features/coupons/user_can_use_coupon_spec.rb
+++ spec/features/coupons/user_can_use_coupon_spec.rb
@@ -21,6 +21,9 @@ describe 'as a user' do
     visit dashboard_path
     click_on 'Generate 10% Off Coupon'
     @coupon_1 = Coupon.last
+    visit dashboard_path
+    click_on 'Generate 10% Off Coupon'
+    @coupon_2 = Coupon.last
     click_on 'Log out'
 
     visit login_path
@@ -39,8 +42,9 @@ describe 'as a user' do
 
   end
 
-  it 'can use a 10% off coupon to check out' do
-    # save_and_open_page
+  it 'can use a 10% off coupon to check out adding many items' do
+    total_pre_discount = @item_1.price + @item_2.price + @item_3.price
+    expect(page).to have_content("Total: $#{total_pre_discount}")
     fill_in :coupon_code, with: @coupon_1.code
     click_button 'Apply'
     expect(current_path).to eq(cart_path)
@@ -48,7 +52,7 @@ describe 'as a user' do
 
     valid_items = @item_1.price + @item_2.price
     total = valid_items - 0.1 * valid_items + @item_3.price
-    expect(page).to have_content("Total: $#{total}")
+    expect(page).to have_content("Total after discount: $#{total}")
 
     visit '/items'
     click_on @item_1.name
@@ -57,10 +61,25 @@ describe 'as a user' do
 
     valid_items = @item_1.price * 2 + @item_2.price
     total = valid_items - 0.1 * valid_items + @item_3.price
-    expect(page).to have_content("Total: $#{total}")
+    expect(page).to have_content("Total after discount: $#{total}")
   end
-  # it 'no one else can use coupon after someone checks out using coupon' do
-  # end
+  it 'cannot enter another coupon after it is entered' do
+    fill_in :coupon_code, with: @coupon_1.code
+    click_button 'Apply'
+
+    expect(page).to have_no_content('Coupon code')
+  end
+  it 'coupon is updated to used after coupon is used' do
+  end
+  it 'a used coupon cannot be used again' do
+  end
+  it 'a merchant can cancel a coupon' do
+  end
+  it 'a cancelled coupon cannot be used' do
+  end
+  it 'an invalid coupon code cannot be used' do
+  end
+
 end
 
 # a7d920
